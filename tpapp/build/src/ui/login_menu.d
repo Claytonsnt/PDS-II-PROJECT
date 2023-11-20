c33ed6b41eda1b9509e562bfd8de5971133d1a76
Clayton Santos <claytonsnt@hotmail.com>
2023-11-19 23:18:03 -0300

    makefile

---
+++ tpapp/build/src/ui/login_menu.d
@@ -0,0 +1,11 @@
+build/src/ui/login_menu.o: src/ui/login_menu.cpp \
+ include/ui/login_menu.hpp include/ui/menu.hpp include/model/usuario.hpp \
+ include/model/desenvolvedor.hpp
+
+include/ui/login_menu.hpp:
+
+include/ui/menu.hpp:
+
+include/model/usuario.hpp:
+
+include/model/desenvolvedor.hpp:
