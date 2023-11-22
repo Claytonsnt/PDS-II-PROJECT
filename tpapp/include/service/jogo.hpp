86b3100509ffdcd740f9378d2e4c288bb9f48b86
Joao Pimentel <78269014+joaovic97@users.noreply.github.com>
2023-11-21 21:32:26 -0300

    Classe Jogo

--- tpapp/include/service/jogo.hpp
+++ tpapp/include/service/jogo.hpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 
-namespace tpapp:: service {
+namespace tpapp::service {
 class Jogo {
 public:
-    Jogo(const int jogo_id, const std::string nome);
-    std::string nome() const;
+    Jogo(const int jogo_id, const std::string nome, const std::string desenvolvedora, const std::string genero, const double valor, std::string data_lancamento);
     int jogo_id() const;
+    std::string nome() const;
+    std::string desenvolvedora() const;
+    std::string genero() const;
+    double valor() const;
+    std::string data_lancamento() const;
 
 private:
-    std::string _nome;
     int _jogo_id;
+    std::string _nome;
+    std::string _desenvolvedora;
+    std::string _genero;
+    double _valor;
+    std::string _data_lancamento;
 };
 }
